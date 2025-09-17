0d2c471745a187da620e10d8953a636b206e5298
Lucas Sidney <lucassidney1999@gmail.com>
2025-09-17 17:59:03 -0300

    adição de arquivos

--- coffiapp/src/routes.ts
+++ coffiapp/src/routes.ts
@@ -2,6 +2,8 @@ import { Router, Response, Request} from 'express'
 const router = Router();
 
 router.get('/teste', (req:Request, res: Response) => {
+    throw new Error("Testando aplicação")
+
     return res.json({ ok: true})
 })
 
--- coffiapp/src/server.ts
+++ coffiapp/src/server.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response, NextFunction} from 'express'
-
 import { router } from './routes'
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) =>{
+    if(err instanceof Error){
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+})
 
 app.listen(3333, () => console.log("Servidor ON!"))
