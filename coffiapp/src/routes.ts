cec17bad771b56683bfd2f1ba3f3dfa22cabf64b
Lucas Sidney <lucassidney1999@gmail.com>
2025-10-03 10:27:53 -0300

    Criando estrutura

--- coffiapp/src/routes.ts
+++ coffiapp/src/routes.ts
@@ -1,9 +1,14 @@
 import { Router, Response, Request} from 'express'
+
+import { CreateUserController } from './controllers/user/CreateUserController';
 const router = Router();
 
-router.get('/teste', (req:Request, res: Response) => {
+/*router.get('/teste', (req:Request, res: Response) => {
 
     return res.json({ ok: true})
-})
+})*/
+
+//Rotas User
+router.post('/users', new CreateUserController().handle)
 
 export { router }
